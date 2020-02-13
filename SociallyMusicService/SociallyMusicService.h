b1df41bf8c3836d6306cadaaa2fcd6e6927616ae
Zach McGuckin <zachmcguckin@gmail.com>
2020-02-13 08:18:08 -0500

    Initial Commit

---
+++ SociallyMusicService/SociallyMusicService.h
@@ -0,0 +1,19 @@
+//
+//  SociallyMusicService.h
+//  SociallyMusicService
+//
+//  Created by Zach McGuckin on 2/13/20.
+//  Copyright © 2020 Zach McGuckin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SociallyMusicService.
+FOUNDATION_EXPORT double SociallyMusicServiceVersionNumber;
+
+//! Project version string for SociallyMusicService.
+FOUNDATION_EXPORT const unsigned char SociallyMusicServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SociallyMusicService/PublicHeader.h>
+
+
