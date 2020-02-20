146ec6e06aa654fe28666e1aa2928bef80835c7f
Zach McGuckin <zachmcguckin@gmail.com>
2020-02-19 21:08:11 -0500

    Adding id's to artists

--- Sources/SociallyMusicService/Models/SociallyArtist.swift
+++ Sources/SociallyMusicService/Models/SociallyArtist.swift
@@ -10,10 +10,12 @@ import Foundation
 public struct SociallyArtist: Codable {
     
     public let name: String
+    public let id: String
     public let imageURL: String
     
     init(from artist: Artist) {
         self.name = artist.name
+        self.id = artist.id
         self.imageURL = artist.images[0].url.absoluteString
     }
 }
